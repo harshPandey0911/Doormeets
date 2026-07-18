510c3c33a746d2f9a60f4fc5e27e403148fb85dc
harsh <harshpandey09112004@gmail.com>
2026-07-18 17:40:22 +0530

    Vendor Management

--- Backend/assign_all_categories.js
+++ Backend/assign_all_categories.js
@@ -5,14 +5,15 @@ mongoose.connect(process.env.MONGODB_URI).then(async () => {
   const Vendor = require('./models/Vendor');
   const Category = require('./models/Category');
   
-  const allCategories = await Category.find().select('_id');
+  const allCategories = await Category.find().limit(2);
   const categoryIds = allCategories.map(c => c._id.toString());
+  const categoryTitles = allCategories.map(c => c.title);
   
   await Vendor.updateMany({}, {
-    $set: { categories: categoryIds, service: categoryIds }
+    $set: { categories: categoryIds, service: categoryTitles }
   });
   
-  console.log(`Updated all vendors to have all ${categoryIds.length} categories for testing.`);
+  console.log(`Updated all vendors to have only 2 categories for testing. Service is now titles.`);
   
   process.exit(0);
 }).catch(err => {
