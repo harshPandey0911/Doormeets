613c232b599cda89220284ba3b2ba18bc4ad896e
harsh <harshpandey09112004@gmail.com>
2026-07-29 10:59:30 +0530

    DB issue fix

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+
+// Force DNS resolution to use public DNS servers (prevents querySrv ETIMEOUT issues)
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 /**
  * Connect to MongoDB
